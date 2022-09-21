aa67a22204994bdf99b6cc577b6f21f14ed52e2f
Viruzzz <nahuelsinka@gmail.com>
2022-09-20 22:50:32 -0300

    cambios de manera de ver el codigo

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,18 +24,21 @@ async function fetchData(urlApi) {
   try {
     const videos = await fetchData(API);
     let view = `
-      ${videos.items.map(video => `
-      <div class="group relative">
-        <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full" />
-        </div>
-       <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
-           <span aria-hidden="true" class="absolute inset-0"></span>
-           ${video.snippet.title}
-          </h3>
-       </div>
-      </div>`
+      ${videos.items.map(video => {
+        const it= `
+        <div class="group relative">
+          <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full" />
+          </div>
+         <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+             <span aria-hidden="true" class="absolute inset-0"></span>
+             ${video.snippet.title}
+            </h3>
+         </div>
+        </div>`
+        return it
+      }
       ).slice(0, 4).join('')}
       `;
     content.innerHTML = view;
